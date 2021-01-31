8229b6a3f586a1a312d91d7cbb2bf774afb806ac
Danny <danielsedlov@gmail.com>
2021-01-31 14:28:30 -0700

    Added Sexy Scene Transition
    It's super nice

--- Assets/Scripts/Loading/LevelLoader.cs
+++ Assets/Scripts/Loading/LevelLoader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
@@ -18,12 +19,30 @@ public class LevelLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync((int)levelToLoad);
+        StartCoroutine(LoadAsynchronously((int)levelToLoad));
     }
 
-    // Update is called once per frame
-    void Update()
+    void Animate()
     {
-        
+        DOTween.PlayAll();
+    }
+
+    public void KillScene()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+    }
+
+    IEnumerator LoadAsynchronously(int levelIndex) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync((int)levelToLoad, LoadSceneMode.Additive);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            Debug.Log(progress);
+            yield return null;
+        }
+        Animate();
+        //KillScene();
     }
 }
