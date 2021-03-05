1a2c16b41c27e2058e380fd3f29ccf1266ecdee4
Danny <danielsedlov@gmail.com>
2021-03-05 14:21:52 -0400

    Merge resolve

--- Assets/Scripts/Game/FightManager.cs
+++ Assets/Scripts/Game/FightManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:Assets/Scripts/Game/FightManager.cs
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -83,54 +82,3 @@ public class FightManager : MonoBehaviour
         enemy.transform.Find("Sprite").GetComponent<SpriteRenderer>().sprite = enemySprites[info.graphic].image;
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FightManager : MonoBehaviour
-{
-    public static bool playerTurn;
-    public static bool newFight;
-    
-    public GameObject enemy;
-    private GameObject clone;
-
-
-    private static bool lockTurn;
-    void Start()
-    {
-        playerTurn = true;
-        newFight = true;
-        lockTurn = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (newFight)
-            InitFight();
-        if (!lockTurn)
-            HandleTurn();
-    }
-
-    void InitFight()
-    {
-        newFight = false;
-        print("I made an enemy!");
-
-        clone = Instantiate(enemy, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
-    }
-
-    void HandleTurn()
-    {
-        lockTurn = true;
-        if (!playerTurn)
-        {
-            clone.GetComponent<Enemy>().DoTurn();
-            playerTurn = true;
-        }
-        lockTurn = false;
-    }
-}
->>>>>>> Stashed changes:Assets/Scripts/Other/Static/FightManager.cs
