acf7f6cede4e8331db62301885b1c099ec273ba4
Danny <danielsedlov@gmail.com>
2021-03-07 14:01:59 -0400

    Added button functionality
    buttons are now clickable

---
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMagic()
+    {
+        print("majik");
+    }
+
+    public void OnSpell()
+    {
+        print("Spell");
+    }
+
+    public void OnHealth()
+    {
+        print("Health");
+    }
+}
