956f786eba59ee349696854fda82acc618203ebb
Danny <danielsedlov@gmail.com>
2021-03-13 16:57:33 -0400

    Added tooltip with timer to all buttons

--- Assets/Scripts/Game/ToolTipManager.cs
+++ Assets/Scripts/Game/ToolTipManager.cs
@@ -5,26 +5,41 @@ using UnityEngine;
 public class ToolTipManager : MonoBehaviour
 {
 
+    float startTime;
+    bool isHovering;
+
+    float toolTipDelay = 1f;
+
     GameObject toolTip;
     // Start is called before the first frame update
     void Start()
     {
         toolTip = transform.Find("ToolTip").gameObject;
+        isHovering = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isHovering)
+        {
+            if(Time.time - startTime >= toolTipDelay)
+            {
+                toolTip.SetActive(true);
+            }
+        }
     }
 
     public void OnEnterHover()
     {
-        toolTip.SetActive(true);
+        startTime = Time.time;
+        isHovering = true;
+        
     }
 
     public void OnExitHover()
     {
+        isHovering = false;
         toolTip.SetActive(false);
     }
 }
