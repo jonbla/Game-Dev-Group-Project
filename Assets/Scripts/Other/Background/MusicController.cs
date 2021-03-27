bae211e8e2e6a40ade91bf3882a46e7db04eca1f
Danny <danielsedlov@gmail.com>
2021-03-26 22:09:59 -0300

    Added popup prefab

--- Assets/Scripts/Other/Background/MusicController.cs
+++ Assets/Scripts/Other/Background/MusicController.cs
@@ -6,18 +6,18 @@ public class MusicController : MonoBehaviour
 {
     AudioSource[] sources;
 
-    public AudioSource T1Music   { get => sources[0]; }
-    public AudioSource T2Music   { get => sources[1]; }
-    public AudioSource T3Music   { get => sources[2]; }
-    public AudioSource T4Music   { get => sources[3]; }
-    public AudioSource MenuMusic { get => sources[4]; }
-    public AudioSource UpgradeMusic { get => sources[5]; }
+    public AudioSource T1Music          { get => sources[0]; }
+    public AudioSource T2Music          { get => sources[1]; }
+    public AudioSource T3Music          { get => sources[2]; }
+    public AudioSource T4Music          { get => sources[3]; }
+    public AudioSource MenuMusic        { get => sources[4]; }
+    public AudioSource UpgradeMusic     { get => sources[5]; }
 
-    public AudioSource PunchSound { get => sources[6]; }
-    public AudioSource EnemyMagicSound { get => sources[7]; }
+    public AudioSource PunchSound       { get => sources[6]; }
+    public AudioSource EnemyMagicSound  { get => sources[7]; }
     public AudioSource PlayerMagicSound { get => sources[8]; }
-    public AudioSource EnemyPunchSound { get => sources[9]; }
-    public AudioSource HealSound { get => sources[10]; }
+    public AudioSource EnemyPunchSound  { get => sources[9]; }
+    public AudioSource HealSound        { get => sources[10]; }
     // Start is called before the first frame update
     void Start()
     {
